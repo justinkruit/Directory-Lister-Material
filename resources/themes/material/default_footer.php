2a9f8edc3610b4e0ff9134e9bb5369cc28f3ebd5
Justin Kruit <justinkruit@live.nl>
2016-07-18 10:57:55 +0200

    Fixed footer

--- resources/themes/material/default_footer.php
+++ resources/themes/material/default_footer.php
@@ -1,3 +1,4 @@
+<?php if($supportdirectorylister || $supportjustinkruit) { ?>
 <div class="container">
 <div class="divider <? echo $divider; ?>"></div>
 
@@ -5,3 +6,4 @@
     <? if ($supportdirectorylister) {?>Powered by, <a href="http://www.directorylister.com" class="<?php echo $color; ?>-text" target="_blank">Directory Lister</a>.<? } ?>
 	<br class="hide-on-med-and-up"><? if ($supportjustinkruit) {?>Material theme made by <a href="http://justinkruit.com" class="<?php echo $color; ?>-text" target="_blank">Justin Kruit</a><? } ?>
 </div></div>
+<?php } ?>
